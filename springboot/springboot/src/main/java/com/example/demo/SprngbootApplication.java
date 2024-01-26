ce38734be4d14af5ee3d689257760b601b5017c9
waylon123254 <114789672+waylon123254@users.noreply.github.com>
2024-01-26 17:14:32 +0800

    Add files via upload

---
+++ springboot/springboot/src/main/java/com/example/demo/SprngbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprngbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprngbootApplication.class, args);
+    }
+
+}
